eeb5c79d141e73d663db773e0ceed23f94a8ea45
Mozixun <141307807+LFRon@users.noreply.github.com>
2025-06-30 21:27:37 +0800

    Update turnip_builder.sh
    更改日志更正其反映的对应文件名

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -193,7 +193,7 @@ EOF
 		zip -r "$workdir"/turnip-magisk.zip ./* &> /dev/null
 		if ! [ -a "$workdir"/turnip-magisk.zip ];
 			then echo -e "$red-Packing failed!$nocolor" && exit 1
-			else echo -e "$green-All done, the module saved to;$nocolor" && echo "$workdir"/turnip.zip
+			else echo -e "$green-All done, the module saved to;$nocolor" && echo "$workdir"/turnip-magisk.zip
 		fi
 }
 
